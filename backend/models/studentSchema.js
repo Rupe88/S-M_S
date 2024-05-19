566ffa26a7fb1aa3e7d4e741c843da9f82d2a30d
Rupe88 <chyrupesh828@gmail.com>
2024-05-19 14:32:26 +0545

    studentSchema created

---
+++ backend/models/studentSchema.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const studentSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    registrationNumber: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    grade: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Student = mongoose.model("Student", studentSchema);
+module.exports = Student;
