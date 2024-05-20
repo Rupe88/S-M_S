939e76e0aa041df7921047114db1cf1acef2db10
Rupe88 <chyrupesh828@gmail.com>
2024-05-20 18:28:57 +0545

    library schema added

---
+++ backend/models/librarySchema.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const librarySchema = new mongoose.Schema(
+  {
+    bookname: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Library = mongoose.model("Library", librarySchema);
+module.exports = Library;
