dfb47f84e436ae415b757787f8e635c7c9c291c4
Rupe88 <chyrupesh828@gmail.com>
2024-05-20 19:24:28 +0545

    attendance schema is added

---
+++ backend/models/attendanceSchema.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const attendanceSchema = new mongoose.Schema({
+  student: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Student",
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["Present", "Absent", "Absent with apology"],
+    required: true,
+  },
+});
+
+const Attendance = mongoose.model("Attendance", attendanceSchema);
+module.exports = Attendance;
